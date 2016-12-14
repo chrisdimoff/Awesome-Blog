9d03fb8572c1a98a55b2642bb0f63f0afcde3d63
Chris Dimoff <chrisdimoff@gmail.com>
2016-12-14 11:41:44 -0800

    fix current_user bug for recent comments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,15 @@ class HomeController < ApplicationController
   def index
     @recent_posts = Post.all.limit(5).order(created_at: :desc)
     @recent_comments = []
-    current_user.posts.limit(5).order(created_at: :desc).each do |post|
-      post.comments.each do |comment|
-        @recent_comments.push(comment)
+
+    if current_user
+      current_user.posts.limit(5).order(created_at: :desc).each do |post|
+        post.comments.each do |comment|
+          @recent_comments.push(comment)
+        end
       end
     end
+
   end
 
   def about
